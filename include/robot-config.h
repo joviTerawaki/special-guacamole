a815afc52f67fa5a80fa2daff00b0b4be6bc4e54
jobo the hobro <97433307+joviTerawaki@users.noreply.github.com>
2024-01-26 14:22:58 -1000

    Update robot-config.h

--- include/robot-config.h
+++ include/robot-config.h
@@ -3,15 +3,21 @@ using namespace vex;
 extern brain Brain;
 
 // VEXcode devices
-extern drivetrain Drivetrain;
-extern controller Controller1;
+extern motor MotorShooties;
 extern motor Intake;
-extern motor Catapult1;
-extern motor Catapult2;
-extern digital_out IntakePiston1;
-extern digital_out IntakePiston2;
-extern digital_out FlapjackPiston1;
-extern digital_out flapjackPiston2;
+extern controller Controller1;
+extern digital_out Flapjack1;
+extern digital_out Flapjack2;
+extern digital_out Blockah1;
+extern digital_out Blockah2;
+extern motor_group LeftDriveSmart; 
+extern motor_group RightDriveSmart; 
+extern smartdrive Drivetrain;
+extern gps DrivetrainGPS;
+extern inertial DrivetrainInertial;
+extern rotation RotationalSensor;
+extern motor LeftTop;
+extern motor RightTop;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
